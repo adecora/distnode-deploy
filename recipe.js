e9930e198fd802fd0d05951919849a32d0e23a1e
adecora <alejandrodecora@gmail.com>
2024-05-17 23:03:05 +0200

    Adding a test suite and code coverage

--- recipe.js
+++ recipe.js
@@ -3,8 +3,8 @@ module.exports = class Recipe {
         this.id = Number(id);
         this.name = null;
     }
-    async hydratate {  // Pretend DB Lookup
-        this.name = `Recipe #${this.id}`;
+    async hydratate (id) {  // Pretend DB Lookup
+        this.name = `Recipe: #${this.id}`;
     }
     toJSON() {
         return { id: this.id, name: this.name };
